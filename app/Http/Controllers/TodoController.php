927e0dbc03798e9e984ecbf356847e0d952ad77a
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-01-26 16:31:08 +0700

    # feat todo controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -32,11 +32,11 @@ class TodoController extends Controller
                     t.deadline,
                     t.project_id,
                     p.title project_name,
-                    t.name team_name
+                    te.name team_name
                 FROM todos t
                 LEFT JOIN todos_users tou ON tou.user_id = :user_id
                 LEFT JOIN team_user tu ON tu.user_id = :user_id AND tu.team_id = :team_id
-                LEFT JOIN teams t ON t.team_id = tu.team_id
+                LEFT JOIN teams te ON te.team_id = tu.team_id
                 LEFT JOIN projects p ON p.id = t.project_id
                 WHERE t.project_id = :project_id;
             ", [
@@ -51,6 +51,48 @@ class TodoController extends Controller
             }
 
             return response()->json($todos);
+        } catch (Exception $err) {
+            return response()->json(
+                ['message' => 'Error fetching todos. Please try again later.'],
+                500
+            );
+        }
+    }
+
+    public function getTodo(int $todo_id)
+    {
+        try {
+            $user = Auth::user();
+            $user_id = $user->id;
+
+            $todos = DB::select("
+                SELECT
+                    t.id,
+                    t.title todo_title,
+                    t.description,
+                    t.start_time,
+                    t.deadline,
+                    t.project_id,
+                FROM todos t
+                WHERE t.todo_id = :todo_id;
+            ", [
+                'todo_id' => $todo_id,
+            ]);
+
+            $todo = null;
+            if (! empty($todos) && isset($todos[0])) {
+                $todo = $todos[0];
+            } else {
+                return response()->json(
+                    ['message' => 'Todo not found.'],
+                    404,
+                );
+            }
+            
+            $todo->start_time = Carbon::parse($todo->start_time)->setTimezone('Asia/Indonesia')->toDayDateTimeString();
+            $todo->deadline = Carbon::parse($todo->deadline)->setTimezone('Asia/Indonesia')->toDayDateTimeString();
+
+            return response()->json($todo);
         } catch (Exception $err) {
             return response()->json(
                 ['message' => 'Error fetching todos. Please try again later.'],
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function() {
+    
+    Route::group('/todos', function() {
+        Route::get('/all', [TodoController::class, 'getAllTodos']);
+        Route::get('/get/{todo_id}', [TodoController::class, 'getTodo']);
+        Route::post('/create/{todo_id}', [TodoController::class, 'createTodo']);
+        Route::patch('/edit/{todo_id}', [TodoController::class, 'editTodo']);
+        Route::delete('/delete/{todo_id}', [TodoController::class, 'deleteTodo']);
+    });
+});
