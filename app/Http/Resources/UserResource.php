8764a40b2af4fbc490cc3cb16ca3267b6ac0948a
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-01-28 00:43:48 +0700

    # feat UserResource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $s3 = Storage::disk('s3');
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'profile_photo_path' => $s3->url($this->profile_photo_path),
+            'teams' => $this->teams,
+        ];
+    }
+}
