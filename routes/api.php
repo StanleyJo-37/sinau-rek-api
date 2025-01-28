81a617f356adb106016330cf3a4649bb4b5e5fbc
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-01-28 14:19:41 +0700

    # feat auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TodoController;
+use App\Http\Middleware\AcceptJson;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::prefix('/auth')->group(function() {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
 Route::middleware('auth:sanctum')->group(function() {
+
     
     Route::prefix('/todos')->group(function() {
         Route::get('/all', [TodoController::class, 'getAllTodos']);
