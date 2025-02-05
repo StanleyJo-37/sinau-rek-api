66dc9b3dca7ea20ce43dd2feb4dc2edbc5a2140e
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-02-05 22:06:59 +0700

    # feat esp32 verification and authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DeviceAuthRequest;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Resources\UserResource;
+use App\Models\Device;
 use App\Models\User;
 use Exception;
 
@@ -92,6 +94,43 @@ class AuthController extends Controller
                 ],
                 500
             );
+        }
+    }
+
+    public function authDevice(DeviceAuthRequest $request) {
+        try {
+            $request->authenticate();
+
+            return response()->json([
+                'message' => 'Device authenticated.',
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Error getting device.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    static public function verifyDevice(string $mac_address, string $hmac, string $timestamp): ?Device {
+        try {
+            
+            $device = Device::where('mac_address', $mac_address)->first();
+
+            if (! isset($device)) {
+                return null;
+            }
+
+            $message = $timestamp;
+
+            $expectedHMAC = hash_hmac('sha256', $message, $device->secret_key);
+            if (!hash_equals($expectedHMAC, $hmac)) {
+                return null;
+            }
+
+            return $device;
+        } catch (Exception $e) {
+            return null;
         }
     }
 }
